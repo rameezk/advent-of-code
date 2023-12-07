b2d0a456dba36c527abd81394a59c38ef42838a8
Rameez Khan <rameezkhan.sa@gmail.com>
2023-12-07 11:59:49 +0200

    Clean up day 7

--- aoc/2023/2023_d07_p1.py
+++ aoc/2023/2023_d07_p1.py
@@ -62,11 +62,6 @@ if __name__ == "__main__":
     # QQQJA 483
     #         """.strip().splitlines()
 
-    S = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
-    S.reverse()
-
-    T = [""]
-
     H = {}
     for card in cards:
         hand, bid = card.split()
