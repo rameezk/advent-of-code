e475b98eb431607b2704e51b4758adb643f9c58d
Rameez Khan <rameez@rameezkhan.dev>
2024-12-03 17:45:58 +0200

    Add 2024 day 4 prep

---
+++ py/aoc/2024/2024_d04_p1.py
@@ -0,0 +1,13 @@
+from aoc.helper import AOC
+
+import re
+
+
+@AOC.puzzle(year=2024, day=4, part=1)
+def solve():
+    data = AOC.get_data()
+    # AOC.submit_answer(result)
+
+
+if __name__ == "__main__":
+    solve()
