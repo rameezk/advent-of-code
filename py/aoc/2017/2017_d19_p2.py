8d4550001ddf538427fd327ec12465a722e35980
Rameez Khan <rameez@rameezkhan.dev>
2025-12-19 16:17:50 +0200

    Day 19 2017

---
+++ py/aoc/2017/2017_d19_p2.py
@@ -0,0 +1,71 @@
+from aoc.helper import AOC
+
+@AOC.puzzle(2017, 19, 2)
+def solve():
+    data = AOC.get_data().strip('\n')
+    # data = """     |
+    #      |  +--+
+    #      A  |  C
+    #  F---|----E|--+
+    #      |  |  |  D
+    #      +B-+  +--+
+    # """
+
+    lines = data.split('\n')
+    if lines[0] == '':
+        lines = lines[1:]
+    if lines[-1] == '':
+        lines = lines[:-1]
+
+    grid = [list(line) for line in lines]
+
+    max_width = max(len(line) for line in grid)
+    for i in range(len(grid)):
+        grid[i] += [' '] * (max_width - len(grid[i]))
+
+    start_col = None
+    for col in range(len(grid[0])):
+        if grid[0][col] == '|':
+            start_col = col
+            break
+
+    row, col = 0, start_col
+    direction = (1, 0)
+    steps = 0
+
+    while True:
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
+            break
+
+        current = grid[row][col]
+
+        if current == ' ':
+            break
+
+        steps += 1
+
+        if current == '+':
+            dr, dc = direction
+
+            for new_dr, new_dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                if (new_dr, new_dc) == (-dr, -dc):
+                    continue
+                if (new_dr, new_dc) == (dr, dc):
+                    continue
+
+                new_row = row + new_dr
+                new_col = col + new_dc
+
+                if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
+                    if grid[new_row][new_col] != ' ':
+                        direction = (new_dr, new_dc)
+                        break
+
+        row += direction[0]
+        col += direction[1]
+
+    answer = steps
+    AOC.submit_answer(answer)
+
+if __name__ == "__main__":
+    solve()
