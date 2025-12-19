d0344c08b599c14edd280b7d8f4e0ec41f6e96e9
Rameez Khan <rameez@rameezkhan.dev>
2025-12-19 13:29:05 +0200

    Day 8 2016

---
+++ py/aoc/2016/2016_d08_p1.py
@@ -0,0 +1,47 @@
+from aoc.helper import AOC
+
+
+@AOC.puzzle(2016, 8, 1)
+def solve():
+    data = AOC.get_data().strip().splitlines()
+
+#     data = """rect 3x2
+# rotate column x=1 by 1
+# rotate row y=0 by 4
+# rotate column x=1 by 1""".splitlines()
+
+    width = 50
+    height = 6
+#     width = 7
+#     height = 3
+    screen = [[False] * width for _ in range(height)]
+
+    for line in data:
+        if line.startswith("rect"):
+            parts = line.split()[1].split("x")
+            w, h = int(parts[0]), int(parts[1])
+            for y in range(h):
+                for x in range(w):
+                    screen[y][x] = True
+        elif line.startswith("rotate row"):
+            parts = line.split()
+            row = int(parts[2].split("=")[1])
+            shift = int(parts[4])
+            old_row = screen[row][:]
+            for x in range(width):
+                screen[row][(x + shift) % width] = old_row[x]
+        elif line.startswith("rotate column"):
+            parts = line.split()
+            col = int(parts[2].split("=")[1])
+            shift = int(parts[4])
+            old_col = [screen[y][col] for y in range(height)]
+            for y in range(height):
+                screen[(y + shift) % height][col] = old_col[y]
+
+    lit_count = sum(sum(row) for row in screen)
+    print(lit_count)
+    AOC.submit_answer(lit_count)
+
+
+if __name__ == "__main__":
+    solve()
