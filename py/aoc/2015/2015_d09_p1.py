bd041957e44dd65b8dbb2ff7170ae5e9fcfeac9f
Rameez Khan <rameez@rameezkhan.dev>
2025-12-19 11:31:55 +0200

    Day 9 2015

---
+++ py/aoc/2015/2015_d09_p1.py
@@ -0,0 +1,42 @@
+from aoc.helper import AOC
+from itertools import permutations
+
+
+@AOC.puzzle(2015, 9, 1)
+def solve():
+    # data = """London to Dublin = 464
+    # London to Belfast = 518
+    # Dublin to Belfast = 141""".strip().splitlines()
+
+    data = AOC.get_data().strip().splitlines()
+
+    distances = {}
+    cities = set()
+
+    for line in data:
+        parts = line.split(" = ")
+        distance = int(parts[1])
+        locations = parts[0].split(" to ")
+        city1, city2 = locations[0], locations[1]
+
+        cities.add(city1)
+        cities.add(city2)
+
+        distances[(city1, city2)] = distance
+        distances[(city2, city1)] = distance
+
+    min_distance = float('inf')
+
+    for route in permutations(cities):
+        total_distance = 0
+        for i in range(len(route) - 1):
+            total_distance += distances[(route[i], route[i + 1])]
+
+        if total_distance < min_distance:
+            min_distance = total_distance
+
+    AOC.submit_answer(min_distance)
+
+
+if __name__ == "__main__":
+    solve()
