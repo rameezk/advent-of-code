3a6e74de9fe501346766963029d800787865e5e2
Rameez Khan <rameez@rameezkhan.dev>
2025-12-20 13:02:17 +0200

    Day 15 2020

---
+++ py/aoc/2020/2020_d15_p2.py
@@ -0,0 +1,34 @@
+from aoc.helper import AOC
+
+@AOC.puzzle(2020, 15, 2)
+def solve():
+    data = AOC.get_data().strip()
+
+#     data = """0,3,6"""
+
+    starting_numbers = list(map(int, data.split(',')))
+
+    memory = {}
+
+    for i in range(len(starting_numbers) - 1):
+        memory[starting_numbers[i]] = i + 1
+
+    last_spoken = starting_numbers[-1]
+
+    target_turn = 30000000
+
+    for turn in range(len(starting_numbers) + 1, target_turn + 1):
+        if last_spoken in memory:
+            next_number = turn - 1 - memory[last_spoken]
+        else:
+            next_number = 0
+
+        memory[last_spoken] = turn - 1
+        last_spoken = next_number
+
+    answer = last_spoken
+
+    AOC.submit_answer(answer)
+
+if __name__ == "__main__":
+    solve()
