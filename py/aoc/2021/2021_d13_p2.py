799741edd57e0c27114aa0f5387669098efa39e5
Rameez Khan <rameez@rameezkhan.dev>
2025-12-20 15:13:22 +0200

    Day 13 2021

---
+++ py/aoc/2021/2021_d13_p2.py
@@ -0,0 +1,87 @@
+from aoc.helper import AOC
+
+@AOC.puzzle(2021, 13, 2)
+def solve():
+    data = AOC.get_data().strip()
+
+#     data = """6,10
+# 0,14
+# 9,10
+# 0,3
+# 10,4
+# 4,11
+# 6,0
+# 6,12
+# 4,1
+# 0,13
+# 10,12
+# 3,4
+# 3,0
+# 8,4
+# 1,10
+# 2,14
+# 8,10
+# 9,0
+#
+# fold along y=7
+# fold along x=5"""
+
+    lines = data.strip().split('\n')
+
+    dots = set()
+    folds = []
+
+    parsing_dots = True
+    for line in lines:
+        if line == '':
+            parsing_dots = False
+            continue
+
+        if parsing_dots:
+            x, y = map(int, line.split(','))
+            dots.add((x, y))
+        else:
+            parts = line.split()
+            axis_val = parts[2].split('=')
+            axis = axis_val[0]
+            val = int(axis_val[1])
+            folds.append((axis, val))
+
+    for axis, fold_line in folds:
+        new_dots = set()
+
+        if axis == 'y':
+            for x, y in dots:
+                if y < fold_line:
+                    new_dots.add((x, y))
+                else:
+                    new_y = fold_line - (y - fold_line)
+                    new_dots.add((x, new_y))
+        else:
+            for x, y in dots:
+                if x < fold_line:
+                    new_dots.add((x, y))
+                else:
+                    new_x = fold_line - (x - fold_line)
+                    new_dots.add((new_x, y))
+
+        dots = new_dots
+
+    max_x = max(x for x, y in dots)
+    max_y = max(y for x, y in dots)
+
+    grid = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+
+    for x, y in dots:
+        grid[y][x] = '#'
+
+    print("\nPart 2:")
+    for row in grid:
+        print(''.join(row))
+
+    answer = "JRZBLGKH"
+
+    AOC.submit_answer(answer)
+
+if __name__ == "__main__":
+    solve()
